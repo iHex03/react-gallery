54b4c1941f4b4c9c2e2e316efefac5b4551bb900
Marcel Mattos <81328490+iHex03@users.noreply.github.com>
2023-08-16 02:22:57 -0300

    First commit

---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color:#3D3F43;
+    border-radius: 10px;
+    padding: 10px;
+
+    img{
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 100%;
+        display: block;
+        margin-bottom: 10px;
+        border-radius: 10px;
+    }
+`;
